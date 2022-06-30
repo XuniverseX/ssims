acb07153dba4719808edfe5d9e8172f7e125d2d1
xuni <1029805801@qq.com>
2022-07-01 00:19:52 +0800

    init

---
+++ src/main/java/com/common/mapper/CarMapper.java
@@ -0,0 +1,7 @@
+package com.common.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.common.entity.Car;
+
+public interface CarMapper extends BaseMapper<Car> {
+}
---
+++ src/main/java/com/common/service/ICarService.java
@@ -0,0 +1,7 @@
+package com.common.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.common.entity.Car;
+
+public interface ICarService extends IService<Car> {
+}
