9f9980e7aceb9f1f2a4d8ab48a0fd1fd9003c858
xuni <1029805801@qq.com>
2022-07-07 01:37:52 +0800

    终极版1.0

---
+++ src/main/java/com/common/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.common.config;
+
+import com.common.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).excludePathPatterns(
+                "/login.html",
+                "/register.html",
+                "/user/login",
+                "/user/register"
+        );
+    }
+}
--- src/main/java/com/common/interceptor/LoginInterceptor.java
+++ src/main/java/com/common/interceptor/LoginInterceptor.java
@@ -1,14 +1,21 @@
 package com.common.interceptor;
 
+import cn.hutool.core.util.StrUtil;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        String identity = (String) session.getAttribute("identity");
+        if (StrUtil.isBlank(identity)) {
+            return false;
+        }
         return true;
     }
 }
